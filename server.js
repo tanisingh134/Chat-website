d7c7ec052efa53268d568840b9946eef3a5cc69e
tanisingh134 <148522117+tanisingh134@users.noreply.github.com>
2025-09-10 11:38:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://chat-website-ogb4.onrender.com/',
+        origin: 'http://localhost:5000',
         methods: ['GET', 'POST'],
         credentials: true,
     },
